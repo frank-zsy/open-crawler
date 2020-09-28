e009c2483bd6fe67511593e4c7032d03a97aa979
Frankzhaopku <syzhao1988@126.com>
2020-09-28 18:16:07 +0800

    refactor: check field when import
    Signed-off-by: Frankzhaopku <syzhao1988@126.com>

--- app/schedule/unzip-example.ts
+++ app/schedule/unzip-example.ts
@@ -6,30 +6,6 @@ import { join } from 'path';
 /* eslint-disable @typescript-eslint/no-var-requires */
 const dateformat = require('dateformat');
 
-export interface PostLinkItem {
-  row: {
-    $: {
-      Id: string;
-      CreationDate: string;
-      PostId: string;
-      RelatedPostId: string;
-      LinkTypeId: string;
-    };
-  };
-}
-
-export interface TagItem {
-  row: {
-    $: {
-      Id: string;
-      TagName: string;
-      Count: string;
-      ExcerptPostId: string;
-      WikiPostId: string;
-    };
-  };
-}
-
 module.exports = {
 
   schedule: {
@@ -56,42 +32,58 @@ module.exports = {
       };
     }));
 
-    for (let i = 0; i < files.length; i++) {
-      const p = files[i];
-      const table = tables[i];
-      ctx.logger.info(`Start to process ${p}`);
-      // get stream of db.table
-      const stream = await client.getInsertStream(db, table);
-      ctx.logger.info('Get stream done');
-      const filePath = join(ctx.app.config.unzip_example.basePath, p);
-      const f = await unzip.unzip7({
-        path: filePath,
-        logger: (msg: any, ...params: any[]) => {
-          ctx.logger.info(msg, ...params);
-        },
-      });
-      ctx.logger.info(`Extract ${f.length} file(s) from ${filePath}`);
-      await Promise.all(f.map(async path => {
-        let count = 0;
-        await readline(path, async (line, index) => {
-          if (index <= 2) return;
-          try {
-            const item = await parseXml<TagItem>(line);
-            // write item in
-            const insertItem = {
-              ...item.row.$,
-              CreatedAt: dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss', true),
-            };
-            stream.write(insertItem);
-            count = index;
-          } catch {
-            // last line will throw
-          }
+    try {
+
+      for (let i = 0; i < files.length; i++) {
+        const p = files[i];
+        const table = tables[i];
+        ctx.logger.info(`Start to process ${p}`);
+        const filePath = join(ctx.app.config.unzip_example.basePath, p);
+        const f = await unzip.unzip7({
+          path: filePath,
+          logger: (msg: any, ...params: any[]) => {
+            ctx.logger.info(msg, ...params);
+          },
         });
-        ctx.logger.info(`Get ${count} records from ${filePath}`);
-      }));
-      // close stream
-      stream.end();
+        ctx.logger.info(`Extract ${f.length} file(s) from ${filePath}`);
+        await Promise.all(f.map(async path => {
+          let count = 0;
+          let lastRecord: any;
+          // get stream of db.table
+          const stream = await client.getInsertStream(db, table);
+          const fieldSet = new Set<string>();
+          stream.on('error', e => {
+            ctx.logger.error(e);
+            ctx.logger.info(count, lastRecord);
+            ctx.logger.info(fieldSet);
+          });
+          ctx.logger.info('Get stream done');
+          await readline(path, async (line, index) => {
+            if (index <= 2) return;
+            try {
+              const item = await parseXml<any>(line);
+              // write item in
+              const insertItem = {
+                ...item.row.$,
+                CreatedAt: dateformat(new Date(), 'yyyy-mm-dd HH:MM:ss', true),
+              };
+              stream.write(insertItem);
+              for (const i of Object.keys(item.row.$)) {
+                fieldSet.add(i);
+              }
+              count = index;
+              lastRecord = insertItem;
+            } catch {
+              // last line will throw
+            }
+          });
+          ctx.logger.info(`Get ${count} records from ${filePath}`);
+          // close stream
+          stream.end();
+        }));
+      }
+    } catch (e) {
+      ctx.logger.error(e);
     }
   },
 

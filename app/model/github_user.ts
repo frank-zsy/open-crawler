15db729e451b64449b3fefb02be2ccc14d40c07d
frank-zsy <syzhao1988@126.com>
2022-05-06 17:23:07 +0800

    fix: add unique index for mongo repo and user table
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- app/model/github_user.ts
+++ app/model/github_user.ts
@@ -1,8 +1,13 @@
 import { Application } from 'egg';
 
-export default (app: Application) => app.mongoose.model('github_user', new app.mongoose.Schema({
-  name: { type: String },
-  lastUpdatedAt: { type: Date },
-  nextUpdateAt: { type: Date },
-  info: { type: Array },
-}));
+export default (app: Application) => {
+  const schema = new app.mongoose.Schema({
+    name: { type: String },
+    lastUpdatedAt: { type: Date },
+    nextUpdateAt: { type: Date },
+    info: { type: Array },
+  });
+  // create unique index on name for batch insert
+  schema.index({ name: 1 }, { unique: true });
+  return app.mongoose.model('github_user', schema);
+};

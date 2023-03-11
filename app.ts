7871d859f082e7b7b55fd96ed421dd3e3a8d4e6d
frank-zsy <syzhao1988@126.com>
2023-03-11 09:46:49 +0800

    feat: gitlab test
    Signed-off-by: frank-zsy <syzhao1988@126.com>

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import { Application } from 'egg';
-import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { appendFileSync, existsSync, readFileSync, writeFileSync } from 'fs';
 import dateformat = require('dateformat');
+import { EOL } from 'os';
 
 class Cache {
   private localPath = './local_data/_cache';
@@ -37,5 +38,57 @@ export default class AppBootHook {
 
   public async didReady() {
     process.on('uncaughtException', e => this.app.logger.error(e));
+
+    const gitlabToken = 'glpat-164y7zdk3SoqFZqvjQMX';
+
+    const url = 'https://gitlab.com/api/v4/projects';
+
+    const projectIds = new Set<string>();
+    const projectIdsFile = './local_data/gitlab_project_ids';
+
+    const ctx = this.app.createAnonymousContext();
+    let maxId = 0;
+    if (existsSync(projectIdsFile)) {
+      await ctx.service.core.utils.readline(projectIdsFile, async line => {
+        projectIds.add(line);
+        const id = parseInt(line);
+        if (id > maxId) maxId = id;
+      });
+    } else {
+      writeFileSync(projectIdsFile, '');
+    }
+    const baseQs = {
+      access_token: gitlabToken,
+      simple: true,
+      sort: 'asc',
+      per_page: 100,
+    };
+    this.app.logger.info(`${projectIds.size} project ids has been loaded, max id is ${maxId}`);
+    ctx.service.core.requestExecutor.setOption({
+      options: [{
+        url,
+        qs: {
+          ...baseQs,
+          id_after: maxId,
+        },
+      }],
+      postProcessor: async (_res, body) => {
+        try {
+          const projects = JSON.parse(body);
+          projects.forEach(p => appendFileSync(projectIdsFile, `${p.id}${EOL}`));
+          ctx.service.core.requestExecutor.appendOptions({
+            url,
+            qs: {
+              ...baseQs,
+              id_after: projects[projects.length - 1].id,
+            },
+          });
+        } catch (e) {
+          this.app.logger.error(_res, body);
+        }
+      },
+    });
+    await ctx.service.core.requestExecutor.start();
+
   }
 }
